4a76d2329e08cb1fd4bf13b3c4dbc3f213a1b772
Jose Manuel Cantera <jose.cantera@iota.org>
2021-06-14 18:50:49 +0200

    Additional tests

--- test/anchor.test.ts
+++ test/anchor.test.ts
@@ -6,17 +6,29 @@ describe("Anchor Messages", () => {
     const network = "https://api.lb-0.testnet.chrysalis2.com";
 
     test("Anchor a message to the initial anchorage", async () => {
-        const channel = await IotaAnchoringChannel.create(network).bind();
-        expect(channel.seed).toBeDefined();
+        const anchoringChannel = await IotaAnchoringChannel.create(network).bind();
+        expect(anchoringChannel.seed).toBeDefined();
+        expect(anchoringChannel.channelID).toBeDefined();
 
-        const announceMsgID = channel.announceMsgID;
+        const announceMsgID = anchoringChannel.announceMsgID;
 
-        console.log(announceMsgID);
-
-        const result = await channel.anchor(message, announceMsgID);
+        const result = await anchoringChannel.anchor(message, announceMsgID);
 
         expect(result.anchorageID).toBe(announceMsgID);
         expect(result.channel.split(":")[1]).toEqual(result.anchorageID);
         expect(result.msgID).toBeDefined();
+    });
+
+    test("Anchor a message to an anchorage point different than announce", async () => {
+        const channel = await IotaAnchoringChannel.create(network).bind();
+
+        const announceMsgID = channel.announceMsgID;
+        const result = await channel.anchor(message, announceMsgID);
+
+        const result2 = await channel.anchor(message, result.msgID);
+
+        expect(result2.anchorageID).toBe(result.msgID);
+        expect(result2.channel.split(":")[1]).toEqual(result.anchorageID);
+        expect(result2.msgID).toBeDefined();
     });
 });
