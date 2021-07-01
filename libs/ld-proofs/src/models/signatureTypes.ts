ed977aceaa3afe403b7b9386acfe6bc9b4e86e8d
Jose Manuel Cantera <jose.cantera@iota.org>
2021-07-01 09:48:30 +0200

    Signature Types concrete to be exposed to JS

--- libs/ld-proofs/src/models/signatureTypes.ts
+++ libs/ld-proofs/src/models/signatureTypes.ts
@@ -1,4 +1,4 @@
-export abstract class SignatureTypes {
+export class SignatureTypes {
     public static ED25519_2018 = "Ed25519Signature2018";
 
     public static JCS_ED25519_2020 = "JcsEd25519Signature2020";
