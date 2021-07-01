c197f93605cab7871ee35fe55fe09d23192ad95c
Jose Manuel Cantera <jose.cantera@iota.org>
2021-07-01 12:22:49 +0200

    Add extra tests to debug

--- libs/ld-proofs/test/ldProofVerifier.test.ts
+++ libs/ld-proofs/test/ldProofVerifier.test.ts
@@ -49,22 +49,29 @@ describe("Verify IOTA Linked Data Proofs", () => {
   const method = "key";
 
   let singleIotaLdProof: IIotaLinkedDataProof;
+  let singleIotaLdProofJsonLd: IIotaLinkedDataProof;
   let chainIotaLdProofJsonLd: IIotaLinkedDataProof[];
 
   beforeAll(async () => {
     const signer = await IotaSigner.create(did, node);
     const channel = await IotaAnchoringChannel.create(undefined, node).bind();
     const ldProofGenerator = IotaLdProofGenerator.create(channel, signer);
-
     singleIotaLdProof = await ldProofGenerator.generate(jsonDocument, {
       verificationMethod: method,
       secret: privateKey,
       anchorageID: channel.firstAnchorageID
     });
 
+    const channel3 = await IotaAnchoringChannel.create(undefined, node).bind();
+    const ldProofGenerator3 = IotaLdProofGenerator.create(channel3, signer);
+    singleIotaLdProofJsonLd = await ldProofGenerator3.generateLd(jsonLdDocument1, {
+      verificationMethod: method,
+      secret: privateKey,
+      anchorageID: channel3.firstAnchorageID
+    });
+
     const channel2 = await IotaAnchoringChannel.create(undefined, node).bind();
     const ldProofGenerator2 = IotaLdProofGenerator.create(channel2, signer);
-
     chainIotaLdProofJsonLd = await ldProofGenerator2.generateChainLd(documentChain, {
       verificationMethod: method,
       secret: privateKey,
@@ -80,6 +87,32 @@ describe("Verify IOTA Linked Data Proofs", () => {
 
     const result = await IotaLdProofVerifier.verifyJson(documentToVerify, { node });
     expect(result).toBe(true);
+  });
+
+  test("should fail verification of a single Linked Data Proof. JSON", async () => {
+    const documentToVerify: IJsonAnchoredDocument = {
+      ...JSON.parse(JSON.stringify(jsonDocument)),
+      proof: singleIotaLdProof
+    };
+
+    // A change to the document to force verification fail
+    documentToVerify.age = 45;
+
+    const result = await IotaLdProofVerifier.verifyJson(documentToVerify, { node });
+    expect(result).toBe(false);
+  });
+
+  test("should fail verification of a single Linked Data Proof. JSON-LD", async () => {
+    const documentToVerify: IJsonAnchoredDocument = {
+      ...JSON.parse(JSON.stringify(jsonLdDocument1)),
+      proof: singleIotaLdProofJsonLd
+    };
+
+    // A change to the document to force verification fail
+    documentToVerify.member2 = 1234;
+
+    const result = await IotaLdProofVerifier.verifyJsonLd(documentToVerify, { node });
+    expect(result).toBe(false);
   });
 
   test("should verify a chain of Linked Data Proofs. JSON-LD. Strict", async () => {
@@ -99,6 +132,28 @@ describe("Verify IOTA Linked Data Proofs", () => {
     ];
     const result = await IotaLdProofVerifier.verifyJsonLdChain(documentsToVerify, { node, strict: true });
     expect(result).toBe(true);
+  });
+
+  test("should fail verification of a chain of Linked Data Proofs. JSON-LD. Integrity fail", async () => {
+    const doc1 = JSON.parse(JSON.stringify(jsonLdDocument1));
+    doc1.age = 45;
+
+    const documentsToVerify: IJsonAnchoredDocument[] = [
+      {
+        ...doc1,
+        proof: chainIotaLdProofJsonLd[0]
+      },
+      {
+        ...jsonLdDocument2,
+        proof: chainIotaLdProofJsonLd[1]
+      },
+      {
+        ...jsonLdDocument3,
+        proof: chainIotaLdProofJsonLd[2]
+      }
+    ];
+    const result = await IotaLdProofVerifier.verifyJsonLdChain(documentsToVerify);
+    expect(result).toBe(false);
   });
 
   test("should fail verification of a chain of Linked Data Proofs. JSON-LD. Strict", async () => {
