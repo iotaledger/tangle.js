7af06601bc58b9c2f9eb537d81dc12d4649e6b0c
Jose Manuel Cantera <jose.cantera@iota.org>
2022-05-11 17:35:19 +0200

    Remove unneeded content

--- libs/anchors/scripts/after-test.sh
+++
@@ -1,3 +0,0 @@
-mv src/helpers/initializationHelper.ts src/helpers/initializationHelper-jest.ts
-cp src/helpers/initializationHelper-web.ts src/helpers/initializationHelper.ts
-rm src/helpers/initializationHelper-web.ts
