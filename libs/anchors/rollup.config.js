472897bceb2717225cab83f41f90c0f6a89e2480
Jose Manuel Cantera <jose.cantera@iota.org>
2022-05-12 08:45:51 +0200

    All LD Proofs test now passing

--- libs/anchors/rollup.config.js
+++ libs/anchors/rollup.config.js
@@ -1,42 +1,49 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import packageDetails from "./package.json";
+import copy from "rollup-plugin-copy";
 
 const plugins = [
-    commonjs({
-        ignoreDynamicRequires: true
-    }),
-    resolve({
-        preferBuiltins: true,
-        browser: process.env.BROWSER
-    })
+  commonjs({
+    ignoreDynamicRequires: true,
+  }),
+  resolve({
+    preferBuiltins: true,
+    browser: process.env.BROWSER,
+  }),
+  copy({
+    targets: [
+      { src: '../../node_modules/@iota/streams/node/streams_bg.wasm', dest: 'dist/cjs' }
+    ]
+  })
 ];
 
 const globs = {};
 for (const dep in packageDetails.dependencies) {
-    globs[dep] = dep;
+  globs[dep] = dep;
 }
 
 export default {
-    input: `./es/index.js`,
-    output: {
-        file: `dist/cjs/index.js`,
-        format: "cjs",
-        name: packageDetails.name
-            .split("-")
-            .map(p => p[0].toUpperCase() + p.slice(1))
-            .join(""),
-        compact: false,
-        sourcemap: "inline",
-        exports: "auto",
-        globals: globs
-    },
-    external: ["fs/promises"].concat(Object.keys(globs)),
-    onwarn: message => {
-        if (!["EMPTY_BUNDLE"].includes(message.code)) {
-            console.error(message);
-            process.exit(1);
-        }
-    },
-    plugins
+  inlineDynamicImports: true,
+  input: `./es/index.js`,
+  output: {
+    file: `dist/cjs/index.js`,
+    format: "cjs",
+    name: packageDetails.name
+      .split("-")
+      .map((p) => p[0].toUpperCase() + p.slice(1))
+      .join(""),
+    compact: false,
+    sourcemap: "inline",
+    exports: "auto",
+    globals: globs,
+  },
+  external: ["fs/promises"].concat(Object.keys(globs)),
+  onwarn: (message) => {
+    if (!["EMPTY_BUNDLE"].includes(message.code)) {
+      console.error(message);
+      process.exit(1);
+    }
+  },
+  plugins,
 };
