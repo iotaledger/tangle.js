8a6ce4ca5667f6d0bdefd69421755228d73ce927
Jose Manuel Cantera <jose.cantera@iota.org>
2023-04-18 13:13:31 +0200

    Rename

--- examples-stardust-inx/src/getTrailLatestState.ts
+++ examples-stardust-inx/src/getTrailLatestState.ts
@@ -10,9 +10,9 @@ const { PLUGIN_ENDPOINT, TOKEN } = process.env;
 const trailID = "urn:trail:iota:tst:0x4f2efd3e41ba0f7e25b559f3a0ee6063ded47fa87ec496b6d4bc5cdefe3b6ffb";
 
 async function run() {
-    const didContent = await get(`${PLUGIN_ENDPOINT}/trails/${encodeURIComponent(trailID)}`, TOKEN);
+    const trail = await get(`${PLUGIN_ENDPOINT}/trails/${encodeURIComponent(trailID)}`, TOKEN);
 
-    console.log(didContent);
+    console.log(trail);
 }
 
 run().then(() => console.log("Done")).catch(err => console.error(err));
