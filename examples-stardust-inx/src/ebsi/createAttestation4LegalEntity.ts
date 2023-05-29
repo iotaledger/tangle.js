ba02d575a4cad65fa951ef00f2e93c655f376831
Jose Manuel Cantera <jose.cantera@iota.org>
2023-05-29 15:41:48 +0200

    Legal Entity Attestation

--- examples-stardust-inx/src/ebsi/createAttestation4LegalEntity.ts
+++ examples-stardust-inx/src/ebsi/createAttestation4LegalEntity.ts
@@ -7,12 +7,16 @@ import {
     IotaDocument, IotaIdentityClient
     , IotaDID,
     ProofPurpose,
-    IotaDIDUrl
+    IotaDIDUrl,
+    Timestamp
 } from "@iota/identity-wasm/node/index.js";
 
 import { Client } from "@iota/client-wasm/node/lib/index.js";
 
 import { Converter } from "@iota/util.js";
+
+// import { IotaSigner, IotaVerifier, SignatureTypes } from "@tangle-js/ld-proofs";
+
 
 import * as dotenv from "dotenv";
 import * as dotenvExpand from "dotenv-expand";
@@ -31,9 +35,14 @@ async function run() {
     });
     const didClient = new IotaIdentityClient(client);
 
+    /*
     const issuerDid = "did:iota:ebsi:0x9c0939fe864d813f4257374146b725e4e0c8a1424a3e2b54a83ffac1c9d94a39";
     const verMethod =  "#sign-1";
     const privateKey = "0x33a6111c4cdaa142b34367b79d1858daa39d56196a6f1261c612c6be90358111ec8db3bb05a78b537b9bb25a34c066572d635cc5dbfd84c0fa8afea37648a356";
+    */
+    const issuerDid = "did:iota:tst:0xe8a6c4bc1bf558d94503c8daf3fc214a04706f0ff63f333ef90e11dd9d8d87f2";
+    const verMethod =  "#sign-1";
+    const privateKey = "0x4ecb2142ae34ece681fd841e9970b53c1c1e42a224c29ca98b1cb8f71ebbf769838d7c8478cd0714fbc1dda74beb62e5e0d4bbcc5e14fbbc8d8ae5f89e74e9d5";
 
     const elements = issuerDid.split(":");
     const did = IotaDID.fromAliasId(elements[elements.length - 1], elements[elements.length - 2]);
@@ -47,25 +56,26 @@ async function run() {
         domainName: "company.example.org"
     };
 
-    // Create an unsigned `UniversityDegree` credential for Alice
-    const unsignedVc = new Credential({
-        context: ["https://europa.eu/schemas/v-id/2020/v1"],
+    const unsignedVc = {
+        "@context": [
+            "https://europa.eu/schemas/v-id/2020/v1",
+            "https://www.w3.org/2018/credentials/v1"
+        ],
         id: "https://example.edu/credentials/3732",
-        type: "VerifiableAttestation",
+        type: ["VerifiableCredential", "VerifiableAttestation"],
         issuer: issuerDid,
-        credentialSubject: subject
-    });
-
-    const credAsJson = unsignedVc.toJSON();
-    // Workaround to add Credential Schema
-    credAsJson["credentialSchema"] = {
-        type: "FullJsonSchemaValidator2021",
-        id: "https://ec.europa.eu/digital-building-blocks/code/projects/EBSI/repos/json-schema/raw/schemas/ebsi-vid/legal-entity/2022-11/schema.json"
-    }
-    credAsJson["issued"] = credAsJson["issuanceDate"];
-    credAsJson["validFrom"] = credAsJson["issuanceDate"];
-
+        credentialSubject: subject,
+        credentialSchema: {
+            type: "FullJsonSchemaValidator2021",
+            id: "https://ec.europa.eu/digital-building-blocks/code/projects/EBSI/repos/json-schema/raw/schemas/ebsi-vid/legal-entity/2022-11/schema.json"
+        },
+        issuanceDate: Timestamp.nowUTC(),
+        issued: Timestamp.nowUTC(),
+        validFrom: Timestamp.nowUTC(),
+    };
+   
     const privateKeyBytes = Converter.hexToBytes(privateKey);
+
 
     // Sign Credential.
     let signedVc;
@@ -73,12 +83,13 @@ async function run() {
     try {
         const options = new ProofOptions({
             purpose: ProofPurpose.assertionMethod(),
-            created: credAsJson["issued"]
+            created: Timestamp.nowUTC()
         });
 
         const iotaUrl = IotaDIDUrl.parse(`${issuerDid}${verMethod}`);
 
-        signedVc = issuerDocument.signData(credAsJson, privateKeyBytes.slice(0, 32), iotaUrl, options);
+        const finalCred = Credential.fromJSON(unsignedVc);
+        signedVc = issuerDocument.signCredential(finalCred, privateKeyBytes.slice(0, 32), iotaUrl, options);
     }
     catch (error) {
         console.error(error);
